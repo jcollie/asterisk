jcollie/asterisk

commit 32a4993c6a25bfd9fcb7cfd653b894edc7653c9b

Merged revisions 144924-144925 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
........
  r144924 | kpfleming | 2008-09-27 10:00:48 -0500 (Sat, 27 Sep 2008) | 6 lines
  
  improve header inclusion process in a few small ways:
  
    - it is no longer necessary to forcibly include asterisk/autoconfig.h; every module already includes asterisk.h as its first header (even before system headers), which serves the same purpose
    - astmm.h is now included by asterisk.h when needed, instead of being forced by the Makefile; this means external modules will build properly against installed headers with MALLOC_DEBUG enabled
    - simplify the usage of some of these headers in the AEL-related stuff in the utils directory
........
  r144925 | kpfleming | 2008-09-27 10:13:30 -0500 (Sat, 27 Sep 2008) | 2 lines
  
  fix some minor issues with rev 144924
........


git-svn-id: http://svn.asterisk.org/svn/asterisk/trunk@144949 f38db490-d61c-443f-a65b-d21fe96a405b

utils/Makefile
@@ -17,7 +17,9 @@ ASTTOPDIR?=..
 .PHONY: clean all uninstall
 
 # to get check_expr, add it to the ALL_UTILS list
-ALL_UTILS:=astman smsq stereorize streamplayer aelparse muted check_expr conf2ael hashtest2 hashtest astcanary refcounter
+ALL_UTILS:=astman smsq stereorize streamplayer muted check_expr hashtest2 hashtest astcanary refcounter
+# aelparse and conf2ael currently not being built because they rely on extconf, which is horribly broken
+# ALL_UTILS:=aelparse conf2ael
 UTILS:=$(ALL_UTILS)
 
 LIBS += $(BKTR_LIB)	# astobj2 with devmode uses backtrace
@@ -82,6 +84,7 @@ md5.c: $(ASTTOPDIR)/main/md5.c
 	@cp $< $@
 astman: astman.o md5.o
 astman: LIBS+=$(NEWT_LIB)
+astman.o: ASTCFLAGS+=-DNO_MALLOC_DEBUG
 
 stereorize: stereorize.o frame.o
 stereorize: LIBS+=-lm
@@ -106,10 +109,12 @@ pval.c: $(ASTTOPDIR)/res/ael/pval.c
 ast_expr2.c: $(ASTTOPDIR)/main/ast_expr2.c
 	@cp $< $@
 
+ast_expr2.o: ASTCFLAGS+=-DSTANDALONE
+
 ast_expr2f.c: $(ASTTOPDIR)/main/ast_expr2f.c
 	@cp $< $@
 
-ast_expr2f.o: ASTCFLAGS+=-DSTANDALONE_AEL -I$(ASTTOPDIR)/main -Wno-unused
+ast_expr2f.o: ASTCFLAGS+=-DSTANDALONE -I$(ASTTOPDIR)/main -Wno-unused
 
 pval.o : ASTCFLAGS+=-DSTANDALONE
 
@@ -118,17 +123,19 @@ check_expr: check_expr.o ast_expr2.o ast_expr2f.o strcompat.o threadstorage.o cl
 aelbison.c: $(ASTTOPDIR)/res/ael/ael.tab.c
 	@cp $< $@
 
-aelbison.o: ASTCFLAGS+=-I$(ASTTOPDIR)/res/ael  -DYYENABLE_NLS=0
+aelbison.o: ASTCFLAGS+=-I$(ASTTOPDIR)/res/ael  -DYYENABLE_NLS=0 -DSTANDALONE
 
 pbx_ael.c: $(ASTTOPDIR)/pbx/pbx_ael.c
 	@cp $< $@
 
-pbx_ael.o: ASTCFLAGS+=-DSTANDALONE_AEL
+pbx_ael.o: ASTCFLAGS+=-DSTANDALONE
 
 aelparse.c: $(ASTTOPDIR)/res/ael/ael_lex.c
 	@cp $< $@
 
-aelparse.o: ASTCFLAGS+=-I$(ASTTOPDIR)/res -DSTANDALONE_AEL -Wno-unused
+ael_main.o: ASTCFLAGS+=-DSTANDALONE
+
+aelparse.o: ASTCFLAGS+=-I$(ASTTOPDIR)/res -DSTANDALONE -Wno-unused
 
 aelparse: aelparse.o aelbison.o pbx_ael.o hashtab.o ael_main.o ast_expr2f.o ast_expr2.o strcompat.o pval.o extconf.o
 
@@ -161,8 +168,8 @@ extconf.o: extconf.c
 conf2ael: conf2ael.o ast_expr2f.o ast_expr2.o hashtab.o aelbison.o aelparse.o pbx_ael.o pval.o extconf.o strcompat.o
 
 testexpr2s: $(ASTTOPDIR)/main/ast_expr2f.c $(ASTTOPDIR)/main/ast_expr2.c $(ASTTOPDIR)/main/ast_expr2.h
-	$(CC) -g -c -I$(ASTTOPDIR)/include -DSTANDALONE_AEL $(ASTTOPDIR)/main/ast_expr2f.c -o ast_expr2f.o
-	$(CC) -g -c -I$(ASTTOPDIR)/include -DSTANDALONE_AEL $(ASTTOPDIR)/main/ast_expr2.c -o ast_expr2.o
+	$(CC) -g -c -I$(ASTTOPDIR)/include -DSTANDALONE $(ASTTOPDIR)/main/ast_expr2f.c -o ast_expr2f.o
+	$(CC) -g -c -I$(ASTTOPDIR)/include -DSTANDALONE $(ASTTOPDIR)/main/ast_expr2.c -o ast_expr2.o
 	$(CC) -g -o testexpr2s ast_expr2f.o ast_expr2.o
 	rm ast_expr2.o ast_expr2f.o 
 	./testexpr2s expr2.testinput

Makefile
@@ -232,8 +232,6 @@ endif
 
 ASTCFLAGS+=-Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations $(DEBUG)
 
-ASTCFLAGS+=-include $(ASTTOPDIR)/include/asterisk/autoconfig.h
-
 ifeq ($(AST_DEVMODE),yes)
   ASTCFLAGS+=-Werror -Wunused -Wundef $(AST_DECLARATION_AFTER_STATEMENT) -Wmissing-format-attribute -Wformat-security #-Wformat=2
 endif

main/Makefile
@@ -123,9 +123,12 @@ ast_expr2.c ast_expr2.h:
 	bison -o $@ -d --name-prefix=ast_yy ast_expr2.y
 
 ast_expr2f.c:
-	flex -o $@ --full ast_expr2.fl # moved the correction of yyfree into the flex input file itself.
-	sed 's@#if __STDC_VERSION__ >= 199901L@#if !defined __STDC_VERSION__ || __STDC_VERSION__ >= 199901L@' ast_expr2f.c > zz
-	mv zz ast_expr2f.c
+	flex -o $@ --full ast_expr2.fl
+	sed 's@#if __STDC_VERSION__ >= 199901L@#if !defined __STDC_VERSION__ || __STDC_VERSION__ >= 199901L@' $@ > $@.fix
+	echo "#include \"asterisk.h\"" > $@
+	echo >> $@
+	cat $@.fix >> $@
+	rm $@.fix
 
 ast_expr2f.o: ASTCFLAGS+=-Wno-unused
 

Makefile.moddir_rules
@@ -18,12 +18,6 @@
 # Also note that we can only set one variable per rule, so we have to
 # repeat the left hand side to set multiple variables.
 
-ifneq ($(findstring MALLOC_DEBUG,$(MENUSELECT_CFLAGS)),)
- ifeq ($(findstring astmm.h,$(ASTCFLAGS)),)
-  ASTCFLAGS+=-include $(ASTTOPDIR)/include/asterisk/astmm.h
- endif
-endif
-
 ifeq ($(findstring LOADABLE_MODULES,$(MENUSELECT_CFLAGS)),)
   ASTCFLAGS+=${GC_CFLAGS}
 endif

include/asterisk.h
@@ -18,14 +18,11 @@
 #ifndef _ASTERISK_H
 #define _ASTERISK_H
 
-/* The include of 'autoconfig.h' is not necessary for any modules that
-   are part of the Asterisk source tree, because the top-level Makefile
-   will forcibly include that header in all compilations before all
-   other headers (even system headers). However, leaving this here will
-   help out-of-tree module builders, and doesn't cause any harm for the
-   in-tree modules.
-*/
 #include "asterisk/autoconfig.h"
+
+#if !defined(NO_MALLOC_DEBUG) && !defined(STANDALONE) && defined(MALLOC_DEBUG)
+#include "asterisk/astmm.h"
+#endif
 
 #include "asterisk/compat.h"
 

main/ast_expr2.c
@@ -143,11 +143,14 @@
  * $FreeBSD: src/bin/expr/expr.y,v 1.16 2000/07/22 10:59:36 se Exp $
  */
 
+#include "asterisk.h"
+
 #include <sys/types.h>
 #include <stdio.h>
-#include "asterisk.h"
+
+#if !defined(STANDALONE)
 ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
-#ifdef STANDALONE
+#else
 #ifndef __USE_ISOC99
 #define __USE_ISOC99 1
 #endif
@@ -436,7 +439,6 @@ static int		to_number __P((struct val *));
 static void		to_string __P((struct val *));
 static struct expr_node *alloc_expr_node(enum node_type);
 static void destroy_arglist(struct expr_node *arglist);
-static int is_really_num(char *str);
 
 /* uh, if I want to predeclare yylex with a YYLTYPE, I have to predeclare the yyltype... sigh */
 typedef struct yyltype
@@ -2502,61 +2504,6 @@ is_zero_or_null (struct val *vp)
 	/* NOTREACHED */
 }
 
-#ifdef STANDALONE
-
-void ast_log(int level, const char *file, int line, const char *function, const char *fmt, ...)
-{
-	va_list vars;
-	va_start(vars,fmt);
-	
-        printf("LOG: lev:%d file:%s  line:%d func: %s  ",
-                   level, file, line, function);
-	vprintf(fmt, vars);
-	fflush(stdout);
-	va_end(vars);
-}
-
-
-int main(int argc,char **argv) {
-	char s[4096];
-	char out[4096];
-	FILE *infile;
-	
-	if( !argv[1] )
-		exit(20);
-	
-	if( access(argv[1],F_OK)== 0 )
-	{
-		int ret;
-		
-		infile = fopen(argv[1],"r");
-		if( !infile )
-		{
-			printf("Sorry, couldn't open %s for reading!\n", argv[1]);
-			exit(10);
-		}
-		while( fgets(s,sizeof(s),infile) )
-		{
-			if( s[strlen(s)-1] == '\n' )
-				s[strlen(s)-1] = 0;
-			
-			ret = ast_expr(s, out, sizeof(out),NULL);
-			printf("Expression: %s    Result: [%d] '%s'\n",
-				   s, ret, out);
-		}
-		fclose(infile);
-	}
-	else
-	{
-		if (ast_expr(argv[1], s, sizeof(s), NULL))
-			printf("=====%s======\n",s);
-		else
-			printf("No result\n");
-	}
-}
-
-#endif
-
 #undef ast_yyerror
 #define ast_yyerror(x) ast_yyerror(x, YYLTYPE *yylloc, struct parse_io *parseio)
 
@@ -2581,6 +2528,7 @@ static void destroy_arglist(struct expr_node *arglist)
 	}
 }
 
+#if !defined(STANDALONE)
 static char *compose_func_args(struct expr_node *arglist)
 {
 	struct expr_node *t = arglist;
@@ -2630,7 +2578,7 @@ static int is_really_num(char *str)
 	else
 		return 0;
 }
-
+#endif
 
 static struct val *op_func(struct val *funcname, struct expr_node *arglist, struct ast_channel *chan)
 {

main/ast_expr2.fl
@@ -22,12 +22,10 @@
  * \brief Dialplan Expression Lexical Scanner
  */
 
-#include "asterisk.h"
-
 #include <sys/types.h>
 #include <stdio.h>
 
-#ifndef STANDALONE
+#if !defined(STANDALONE)
 ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #else
 #ifndef __USE_ISOC99
@@ -276,7 +274,7 @@ int ast_expr(char *expr, char *buf, int length, struct ast_channel *chan)
 			return_value = (res_length <= length) ? res_length : length;
 		} else {
 			if (io.val->u.s)
-#if defined(STANDALONE) || defined(LOW_MEMORY) || defined(STANDALONE_AEL)
+#if defined(STANDALONE) || defined(LOW_MEMORY) || defined(STANDALONE)
 				strncpy(buf, io.val->u.s, length - 1);
 #else /* !STANDALONE && !LOW_MEMORY */
 				ast_copy_string(buf, io.val->u.s, length);

main/ast_expr2.y
@@ -12,11 +12,14 @@
  * $FreeBSD: src/bin/expr/expr.y,v 1.16 2000/07/22 10:59:36 se Exp $
  */
 
+#include "asterisk.h"
+
 #include <sys/types.h>
 #include <stdio.h>
-#include "asterisk.h"
+
+#if !defined(STANDALONE)
 ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
-#ifdef STANDALONE
+#else
 #ifndef __USE_ISOC99
 #define __USE_ISOC99 1
 #endif
@@ -305,7 +308,6 @@ static int		to_number __P((struct val *));
 static void		to_string __P((struct val *));
 static struct expr_node *alloc_expr_node(enum node_type);
 static void destroy_arglist(struct expr_node *arglist);
-static int is_really_num(char *str);
 
 /* uh, if I want to predeclare yylex with a YYLTYPE, I have to predeclare the yyltype... sigh */
 typedef struct yyltype
@@ -648,61 +650,6 @@ is_zero_or_null (struct val *vp)
 	/* NOTREACHED */
 }
 
-#ifdef STANDALONE
-
-void ast_log(int level, const char *file, int line, const char *function, const char *fmt, ...)
-{
-	va_list vars;
-	va_start(vars,fmt);
-	
-        printf("LOG: lev:%d file:%s  line:%d func: %s  ",
-                   level, file, line, function);
-	vprintf(fmt, vars);
-	fflush(stdout);
-	va_end(vars);
-}
-
-
-int main(int argc,char **argv) {
-	char s[4096];
-	char out[4096];
-	FILE *infile;
-	
-	if( !argv[1] )
-		exit(20);
-	
-	if( access(argv[1],F_OK)== 0 )
-	{
-		int ret;
-		
-		infile = fopen(argv[1],"r");
-		if( !infile )
-		{
-			printf("Sorry, couldn't open %s for reading!\n", argv[1]);
-			exit(10);
-		}
-		while( fgets(s,sizeof(s),infile) )
-		{
-			if( s[strlen(s)-1] == '\n' )
-				s[strlen(s)-1] = 0;
-			
-			ret = ast_expr(s, out, sizeof(out),NULL);
-			printf("Expression: %s    Result: [%d] '%s'\n",
-				   s, ret, out);
-		}
-		fclose(infile);
-	}
-	else
-	{
-		if (ast_expr(argv[1], s, sizeof(s), NULL))
-			printf("=====%s======\n",s);
-		else
-			printf("No result\n");
-	}
-}
-
-#endif
-
 #undef ast_yyerror
 #define ast_yyerror(x) ast_yyerror(x, YYLTYPE *yylloc, struct parse_io *parseio)
 
@@ -727,6 +674,7 @@ static void destroy_arglist(struct expr_node *arglist)
 	}
 }
 
+#if !defined(STANDALONE)
 static char *compose_func_args(struct expr_node *arglist)
 {
 	struct expr_node *t = arglist;
@@ -776,7 +724,7 @@ static int is_really_num(char *str)
 	else
 		return 0;
 }
-
+#endif
 
 static struct val *op_func(struct val *funcname, struct expr_node *arglist, struct ast_channel *chan)
 {

commit 06810644f56e7572db8993e98a7bc4104b20c081

fix bugs caused by r144949 when MALLOC_DEBUG is defined
git-svn-id: http://svn.asterisk.org/svn/asterisk/trunk@144950 f38db490-d61c-443f-a65b-d21fe96a405b

agi/Makefile
@@ -29,8 +29,13 @@ all: $(AGIS)
 
 strcompat.c: ../main/strcompat.c
 	@cp $< $@
+strcompat.o: ASTCFLAGS+=-DSTANDALONE
+
+eagi-test.o: ASTCFLAGS+=-DSTANDALONE
 
 eagi-test: eagi-test.o strcompat.o
+
+eagi-sphinx-test.o: ASTCFLAGS+=-DSTANDALONE
 
 eagi-sphinx-test: eagi-sphinx-test.o
 

include/asterisk/astmm.h
@@ -27,6 +27,13 @@
 
 #include "asterisk.h"
 
+/* Include these now to prevent them from being needed later */
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+
 /* Undefine any macros */
 #undef malloc
 #undef calloc

utils/Makefile
@@ -82,52 +82,44 @@ clean:
 
 md5.c: $(ASTTOPDIR)/main/md5.c
 	@cp $< $@
+md5.o: ASTCFLAGS+=-DSTANDALONE
+
 astman: astman.o md5.o
 astman: LIBS+=$(NEWT_LIB)
-astman.o: ASTCFLAGS+=-DNO_MALLOC_DEBUG
+astman.o: ASTCFLAGS+=-DSTANDALONE
 
 stereorize: stereorize.o frame.o
 stereorize: LIBS+=-lm
 
 hashtab.c: $(ASTTOPDIR)/main/hashtab.c
 	@cp $< $@
+hashtab.o: ASTCFLAGS+=-DSTANDALONE
 
 strcompat.c: $(ASTTOPDIR)/main/strcompat.c
 	@cp $< $@
-
-$(ASTTOPDIR)/main/ast_expr2.c:
-	@echo "   [BISON] $(ASTTOPDIR)/main/ast_expr2.y -> $@"
-	@bison -o $@ -d --name-prefix=ast_yy $(ASTTOPDIR)/main/ast_expr2.y
-
-$(ASTTOPDIR)/main/ast_expr2f.c:
-	@echo "   [FLEX] $(ASTTOPDIR)/main/ast_expr2.fl -> $@"
-	@flex -o $@ --full $(ASTTOPDIR)/main/ast_expr2.fl
+strcompat.o: ASTCFLAGS+=-DSTANDALONE
 
 pval.c: $(ASTTOPDIR)/res/ael/pval.c
 	@cp $< $@
+pval.o : ASTCFLAGS+=-DSTANDALONE
 
 ast_expr2.c: $(ASTTOPDIR)/main/ast_expr2.c
 	@cp $< $@
-
 ast_expr2.o: ASTCFLAGS+=-DSTANDALONE
 
 ast_expr2f.c: $(ASTTOPDIR)/main/ast_expr2f.c
 	@cp $< $@
-
 ast_expr2f.o: ASTCFLAGS+=-DSTANDALONE -I$(ASTTOPDIR)/main -Wno-unused
 
-pval.o : ASTCFLAGS+=-DSTANDALONE
-
 check_expr: check_expr.o ast_expr2.o ast_expr2f.o strcompat.o threadstorage.o clicompat.o
+check_expr.o: ASTCFLAGS+=-DSTANDALONE
 
 aelbison.c: $(ASTTOPDIR)/res/ael/ael.tab.c
 	@cp $< $@
-
 aelbison.o: ASTCFLAGS+=-I$(ASTTOPDIR)/res/ael  -DYYENABLE_NLS=0 -DSTANDALONE
 
 pbx_ael.c: $(ASTTOPDIR)/pbx/pbx_ael.c
 	@cp $< $@
-
 pbx_ael.o: ASTCFLAGS+=-DSTANDALONE
 
 aelparse.c: $(ASTTOPDIR)/res/ael/ael_lex.c
@@ -136,32 +128,32 @@ aelparse.c: $(ASTTOPDIR)/res/ael/ael_lex.c
 ael_main.o: ASTCFLAGS+=-DSTANDALONE
 
 aelparse.o: ASTCFLAGS+=-I$(ASTTOPDIR)/res -DSTANDALONE -Wno-unused
-
 aelparse: aelparse.o aelbison.o pbx_ael.o hashtab.o ael_main.o ast_expr2f.o ast_expr2.o strcompat.o pval.o extconf.o
 
 astobj2.c: $(ASTTOPDIR)/main/astobj2.c
 	@cp $< $@
+astobj2.o: ASTCFLAGS+=-DSTANDALONE
 
 utils.c: $(ASTTOPDIR)/main/utils.c
 	@cp $< $@
+utils.o: ASTCFLAGS+=-DSTANDALONE
 
 sha1.c: $(ASTTOPDIR)/main/sha1.c
 	@cp $< $@
+sha1.o: ASTCFLAGS+=-DSTANDALONE
 
 threadstorage.c: $(ASTTOPDIR)/main/threadstorage.c
 	@cp $< $@
+threadstorage.o: ASTCFLAGS+=-DSTANDALONE
 
-hashtest2.o: ASTCFLAGS+=-O0
-
+hashtest2.o: ASTCFLAGS+=-O0 -DSTANDALONE
 hashtest2: hashtest2.o md5.o utils.o astobj2.o sha1.o strcompat.o threadstorage.o clicompat.o
 
 hashtest: hashtest.o md5.o hashtab.o utils.o sha1.o strcompat.o threadstorage.o clicompat.o
-
-hashtest.o: ASTCFLAGS+=-O0
+hashtest.o: ASTCFLAGS+=-O0 -DSTANDALONE
 
 refcounter: refcounter.o md5.o hashtab.o utils.o sha1.o strcompat.o threadstorage.o clicompat.o
-
-refcounter.o: ASTCFLAGS+=-O0
+refcounter.o: ASTCFLAGS+=-O0 -DSTANDALONE
 
 extconf.o: extconf.c
 