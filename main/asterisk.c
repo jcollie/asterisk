jcollie/asterisk

commit 6202c60914d5fe0c36803f62a884b7f83bcf877f

Correctly allow chan_dahdi to compile against older versions of Zaptel. Don't always define HAVE_ZAPTEL_CHANALARMS (since we check if it's defined..) Minor cleanup to make things clear.
(closes issue #13726)
Reported by: tzafrir
Patches:
      dahdi_def.diff uploaded by tzafrir (license 46)


git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/1.4@150557 f38db490-d61c-443f-a65b-d21fe96a405b

main/asterisk.c
@@ -74,7 +74,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #include <pwd.h>
 #include <sys/stat.h>
 
-#if defined(HAVE_ZAPTEL) || defined (HAVE_DAHDI)
+#if defined(HAVE_DAHDI)
 #include <sys/ioctl.h>
 #include "asterisk/dahdi_compat.h"
 #endif

channels/chan_dahdi.c
@@ -3668,7 +3668,7 @@ static int get_alarms(struct dahdi_pvt *p)
 {
 	int res;
 	struct dahdi_spaninfo zi;
-#if defined(HAVE_DAHDI) || defined(HAVE_ZAPTEL_CHANALARMS)
+#if !defined(HAVE_ZAPTEL) || defined(HAVE_ZAPTEL_CHANALARMS)
 	/*
 	 * The conditional compilation is needed only in asterisk-1.4 for
 	 * backward compatibility with old zaptel drivers that don't have
@@ -3687,7 +3687,7 @@ static int get_alarms(struct dahdi_pvt *p)
 	}
 	if (zi.alarms != DAHDI_ALARM_NONE)
 		return zi.alarms;
-#if defined(HAVE_DAHDI) || defined(HAVE_ZAPTEL_CHANALARMS)
+#if !defined(HAVE_ZAPTEL) || defined(HAVE_ZAPTEL_CHANALARMS)
 	/* No alarms on the span. Check for channel alarms. */
 	if ((res = ioctl(p->subs[SUB_REAL].dfd, DAHDI_GET_PARAMS, &params)) >= 0)
 		return params.chan_alarms;

configure
@@ -12126,11 +12126,13 @@ _ACEOF
 cat confdefs.h >>conftest.$ac_ext
 cat >>conftest.$ac_ext <<_ACEOF
 /* end confdefs.h.  */
-#include <stdio.h>
+#include <sys/types.h> /* for off_t */
+     #include <stdio.h>
 int
 main ()
 {
-return fseeko (stdin, 0, 0) && (fseeko) (stdin, 0, 0);
+int (*fp) (FILE *, off_t, int) = fseeko;
+     return fseeko (stdin, 0, 0) && fp (stdin, 0, 0);
   ;
   return 0;
 }
@@ -12170,11 +12172,13 @@ cat confdefs.h >>conftest.$ac_ext
 cat >>conftest.$ac_ext <<_ACEOF
 /* end confdefs.h.  */
 #define _LARGEFILE_SOURCE 1
-#include <stdio.h>
+#include <sys/types.h> /* for off_t */
+     #include <stdio.h>
 int
 main ()
 {
-return fseeko (stdin, 0, 0) && (fseeko) (stdin, 0, 0);
+int (*fp) (FILE *, off_t, int) = fseeko;
+     return fseeko (stdin, 0, 0) && fp (stdin, 0, 0);
   ;
   return 0;
 }
@@ -30574,12 +30578,6 @@ echo "${ECHO_T}$ac_cv_member_ZT_PARAMS_chan_alarms" >&6; }
 
 cat >>confdefs.h <<\_ACEOF
 #define HAVE_ZAPTEL_CHANALARMS 1
-_ACEOF
-
-	else
-
-cat >>confdefs.h <<\_ACEOF
-#define HAVE_ZAPTEL_CHANALARMS 0
 _ACEOF
 
 	fi

configure.ac
@@ -1464,8 +1464,6 @@ if test "${USE_ZAPTEL}" != "no" && test "x${PBX_DAHDI}" != "x1"; then
 	CPPFLAGS="${saved_cppflags}"
 	if test "${ac_cv_member_ZT_PARAMS_chan_alarms}" = "yes"; then
 	     AC_DEFINE([HAVE_ZAPTEL_CHANALARMS], 1, [Define if your Zaptel drivers have chan_alarms.])
-	else
-	     AC_DEFINE([HAVE_ZAPTEL_CHANALARMS], 0, [Define if your Zaptel drivers have chan_alarms.])
 	fi
    elif test -n "${ZAPTEL_MANDATORY}"; 
    then

main/channel.c
@@ -37,7 +37,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #include <unistd.h>
 #include <math.h>
 
-#if defined(HAVE_ZAPTEL) || defined (HAVE_DAHDI)
+#if defined(HAVE_DAHDI)
 #include <sys/ioctl.h>
 #include "asterisk/dahdi_compat.h"
 #endif

commit 0adb933b96e5917f165f9be120de65072b684206

Fix the 1.4 branch compile again broken with r150557 when using with Zaptel and not DAHDI
 (closes issue #13740)
 reported by: jmls
 patch by: bweschke



git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/1.4@151100 f38db490-d61c-443f-a65b-d21fe96a405b

main/asterisk.c
@@ -74,9 +74,16 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #include <pwd.h>
 #include <sys/stat.h>
 
-#if defined(HAVE_DAHDI)
+#if defined(HAVE_ZAPTEL) || defined (HAVE_DAHDI)
 #include <sys/ioctl.h>
+#endif
+
+#if defined(HAVE_DAHDI)
 #include "asterisk/dahdi_compat.h"
+#endif
+
+#if defined(HAVE_ZAPTEL)
+#include <zaptel/zaptel.h>
 #endif
 
 #ifdef linux
@@ -2991,7 +2998,7 @@ int main(int argc, char *argv[])
 		int x = 160;
 		fd = open("/dev/zap/timer", O_RDWR);
 		if (fd >= 0) {
-			if (ioctl(fd, DAHDI_TIMERCONFIG, &x)) {
+			if (ioctl(fd, ZT_TIMERCONFIG, &x)) {
 				ast_log(LOG_ERROR, "You have Zaptel built and drivers loaded, but the Zaptel timer test failed to set ZT_TIMERCONFIG to %d.\n", x);
 				exit(1);
 			}